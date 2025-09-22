d78617946129f3ca96562fa531b897210155d808
Royalross <royaltross@gmail.com>
2025-09-21 21:48:37 -0400

    Hello World Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <>
+            <div className="flex justify-center min-h-full ">
+                <h1> Hello Word </h1>
+            </div>
+            <div className="flex justify-center min-h-full ">
+                <h1> From CSE 5234 – Distributed Enterprise Computing </h1>
+            </div>
+        </>
+
+    );
+}
